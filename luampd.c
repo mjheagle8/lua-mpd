f0e495feba08c1ae885b86d4c2e6f840cd8f71c4
mjheagle <mjheagle8@gmail.com>
2013-01-12 12:03:56 -0600

    function to get now playing data

--- luampd.c
+++ luampd.c
@@ -255,6 +255,47 @@ int mpd_state(lua_State *L)
         return 1;
 }
 
+/*
+ * get now playing track data
+ * lua function
+ * argument is connection
+ * return is a table with tag data
+ */
+int mpd_now_playing(lua_State *L)
+{
+        /* get mpd connection from arguments */
+        struct mpd_connection *conn = NULL;
+        if (lua_islightuserdata(L, 1) == 1)
+                 conn = (struct mpd_connection *)lua_topointer(L, 1);
+        else
+                return 0;
+
+        /* get current song from mpd */
+        struct mpd_song *song = mpd_run_current_song(conn);
+
+        /* create table for results */
+        lua_newtable(L);
+
+        /* iterate through tags and add them to the table */
+        lua_table_push_str(L, "title",  mpd_song_get_tag(song, MPD_TAG_TITLE, 0));
+        lua_table_push_str(L, "artist", mpd_song_get_tag(song, MPD_TAG_ARTIST, 0));
+        lua_table_push_str(L, "album_artist", mpd_song_get_tag(song, MPD_TAG_ALBUM_ARTIST, 0));
+        lua_table_push_str(L, "album",  mpd_song_get_tag(song, MPD_TAG_ALBUM, 0));
+        lua_table_push_str(L, "track",  mpd_song_get_tag(song, MPD_TAG_TRACK, 0));
+        lua_table_push_str(L, "name",   mpd_song_get_tag(song, MPD_TAG_NAME, 0));
+        lua_table_push_str(L, "genre",  mpd_song_get_tag(song, MPD_TAG_GENRE, 0));
+        lua_table_push_str(L, "date",   mpd_song_get_tag(song, MPD_TAG_DATE, 0));
+        lua_table_push_str(L, "composer",  mpd_song_get_tag(song, MPD_TAG_COMPOSER, 0));
+        lua_table_push_str(L, "performer",  mpd_song_get_tag(song, MPD_TAG_PERFORMER, 0));
+        lua_table_push_str(L, "comment",  mpd_song_get_tag(song, MPD_TAG_COMMENT, 0));
+        lua_table_push_str(L, "disc",  mpd_song_get_tag(song, MPD_TAG_DISC, 0));
+
+        /* free song struct */
+        mpd_song_free(song);
+
+        return 1;
+}
+
 /* index of functions */
 static const struct luaL_Reg mpd[] =
 {
@@ -265,6 +306,7 @@ static const struct luaL_Reg mpd[] =
         {"prev",                mpd_prev},
         {"toggle",              mpd_toggle},
         {"state",               mpd_state},
+        {"now_playing",         mpd_now_playing},
         {"free_connection",     mpd_free_connection},
         {NULL,          NULL}
 };
--- test.lua
+++ test.lua
@@ -21,5 +21,12 @@ for k, v in pairs(state) do
     print(string.format('  %s: %s', tostring(k), tostring(v)))
 end
 
+print('now playing:')
+np = mpd.now_playing(conn)
+print(np)
+for k, v in pairs(np) do
+    print(string.format('  %s: %s', tostring(k), tostring(v)))
+end
+
 mpd.free_connection(conn)
 print('done')
