f2914e6bb6aaba601faef93ef78df7a6e2d22a65
mjheagle <mjheagle8@gmail.com>
2013-01-19 11:22:10 -0600

    printtable function in test.lua to robustly handle printing tables

--- test.lua
+++ test.lua
@@ -10,6 +10,13 @@ else
     os.exit(1)
 end
 
+local function printtable(t)
+    if type(t) ~= type({}) then return end
+    for k, v in pairs(t) do
+        print(string.format('  %s: %s', tostring(k), tostring(v)))
+    end
+end
+
 print('attempting mpd connection...')
 conn = mpd.connect('localhost', 6600, 1000)
 print(conn)
@@ -17,29 +24,25 @@ print(conn)
 print('state:')
 state = mpd.state(conn)
 print(state)
-for k, v in pairs(state) do
-    print(string.format('  %s: %s', tostring(k), tostring(v)))
-end
+printtable(state)
 
 print('now playing:')
 np = mpd.now_playing(conn)
 print(np)
-for k, v in pairs(np) do
-    print(string.format('  %s: %s', tostring(k), tostring(v)))
-end
+printtable(np)
 
 print('stats:')
 stats = mpd.stats(conn)
 print(stats)
-for k, v in pairs(stats) do
-    print(string.format('  %s: %s', tostring(k), tostring(v)))
-end
+printtable(stats)
 
 print('playlist:')
 pl = mpd.playlist(conn)
 print(pl)
-for k, v in pairs(pl) do
-    print(string.format('  %s: %s - %s', tostring(k), tostring(v.artist), tostring(v.title)))
+if type(pl) == type({}) then
+    for k, v in pairs(pl) do
+        print(string.format('  %s: %s - %s', tostring(k), tostring(v.artist), tostring(v.title)))
+    end
 end
 
 mpd.free_connection(conn)
